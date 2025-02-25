27b81ff8e4644dec15f72f94e8a0d3d4f6a73d0a
ustymenko-d <ustymenko.denys@gmail.com>
2025-02-25 21:45:04 +0200

    Add ThemeToggle to AuthLayout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,4 @@
+import ThemeToggle from '@/components/Theme/ThemeToggle'
 import { buttonVariants } from '@/components/ui/button'
 import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
@@ -5,14 +6,18 @@ import Link from 'next/link'
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className='container mx-auto py-4 px-2 flex flex-col grow w-full '>
-			<Link
-				className={buttonVariants({
-					variant: 'outline',
-					size: 'icon',
-				})}
-				href='/'>
-				<ChevronLeft />
-			</Link>
+			<div className='flex items-center gap-x-2'>
+				<Link
+					className={buttonVariants({
+						variant: 'outline',
+						size: 'icon',
+					})}
+					href='/'>
+					<ChevronLeft />
+				</Link>
+
+				<ThemeToggle />
+			</div>
 
 			<div className='grow flex items-center justify-center'>{children}</div>
 		</div>
