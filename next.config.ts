c7a1965c1d3bdd03b465f881a6a82a9a0c066ddb
ustymenko-d <ustymenko.denys@gmail.com>
2025-03-29 19:01:26 +0200

    Next config update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	// reactStrictMode: false,
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: 'https://uptodo-client.vercel.app',
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'X-Requested-With, Content-Type, Authorization',
+					},
+					{ key: 'Access-Control-Expose-Headers', value: 'Set-Cookie' },
+				],
+			},
+		]
+	},
 }
 
 export default nextConfig
