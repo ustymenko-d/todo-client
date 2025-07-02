84d8935d06072dea76b94e73b0e17d4b1271d047
ustymenko-d <ustymenko.denys@gmail.com>
2025-07-02 18:58:56 +0300

    Table task search: loader fix

--- src/components/pages/Table/components/Table/components/Head.tsx
+++ src/components/pages/Table/components/Table/components/Head.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import debounce from 'lodash.debounce'
-import { Plus, Settings2 } from 'lucide-react'
+import { Loader2, Plus, Settings2 } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { ChangeEvent, useEffect, useMemo, useTransition } from 'react'
 
@@ -14,7 +14,6 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
-import Loader from '@/components/ui/Loader'
 import useBreakpoints from '@/hooks/useBreakpoints'
 import useAppStore from '@/store/store'
 
@@ -89,14 +88,19 @@ const Head = ({ table }: ITableComponentProps) => {
 
 	return (
 		<div className='flex items-center gap-4 py-4'>
-			<div className='flex items-center gap-2'>
+			<div className='flex items-center gap-2 relative'>
 				<Input
-					className='max-w-sm'
+					className='max-w-sm pe-8'
 					value={searchTerm}
 					onChange={(e) => handleChange(e)}
 					placeholder='Search by title...'
 				/>
-				{isPending && <Loader text='Searching' />}
+				{isPending && (
+					<Loader2
+						strokeWidth={1.25}
+						className='absolute right-2 w-5 h-5 text-gray-500 animate-spin'
+					/>
+				)}
 			</div>
 
 			<Button onClick={() => openTaskEditor('create', null)}>
