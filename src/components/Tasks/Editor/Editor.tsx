21309b98287b51060cef9fa3548ec3d669a18874
ustymenko-d <ustymenko.denys@gmail.com>
2025-07-02 14:37:05 +0300

    Refactor Editor component to simplify title and description logic

--- src/components/Tasks/Editor/Editor.tsx
+++ src/components/Tasks/Editor/Editor.tsx
@@ -14,7 +14,23 @@ import EditorForm from './components/EditorForm'
 const Editor = () => {
 	const open = useAppStore((s) => s.taskEditorSettings.open)
 	const mode = useAppStore((s) => s.taskEditorSettings.mode)
+	const selectedTask = useAppStore((s) => s.taskEditorSettings.target)
 	const closeEditor = useAppStore((s) => s.closeTaskEditor)
+
+	const isEditing = mode === 'edit'
+	const isCreating = mode === 'create'
+	const isSubtask = isCreating && !!selectedTask
+
+	const editorConfig = {
+		title: isEditing
+			? 'Edit task'
+			: isSubtask
+			? 'Create new subtask'
+			: 'Create new task',
+		description: isEditing
+			? 'Update the task information below. All changes will be saved once you click "Edit task".'
+			: 'Complete the form below to add a new task. Title is required, other fields are optional.',
+	}
 
 	return (
 		<Dialog
@@ -22,14 +38,8 @@ const Editor = () => {
 			onOpenChange={closeEditor}>
 			<DialogContent className='sm:max-w-[425px]'>
 				<DialogHeader>
-					<DialogTitle>
-						{mode === 'edit' ? 'Edit task' : 'Create new task'}
-					</DialogTitle>
-					<DialogDescription>
-						{mode === 'edit'
-							? 'Update the task information below. All changes will be saved once you click "Edit task".'
-							: 'Complete the form below to add a new task. Title is required, other fields are optional.'}
-					</DialogDescription>
+					<DialogTitle>{editorConfig.title}</DialogTitle>
+					<DialogDescription>{editorConfig.description}</DialogDescription>
 				</DialogHeader>
 				<EditorForm />
 			</DialogContent>
